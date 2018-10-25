4d279fb568101f9d96c83ed08c86d9073dcf2acc
yangyao <yao.yang@pactera.com>
2018-10-25 13:15:47 +0800

    新增Eureka双节点集群，并测试

--- tbo-p/tbo-sys/src/main/java/com/chizuko/sys/controller/HelloController.java
+++ tbo-p/tbo-sys/src/main/java/com/chizuko/sys/controller/HelloController.java
@@ -4,8 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -24,8 +23,8 @@ public class HelloController {
 
     @Autowired
     private DiscoveryClient client;
-
-    @RequestMapping(value = "hello", method = RequestMethod.GET)
+//
+    @GetMapping(value = "hello")
     public String index() {
 
         client.getServices().forEach(serviceId -> {
